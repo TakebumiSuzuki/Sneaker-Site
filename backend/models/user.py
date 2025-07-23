7d59ee6b31fea244110d97a6cd0dceadcc7bfeb7
Takebumi Suzuki <takebumi.suzuki@gmail.com>
2025-07-23 11:44:50 +0700

    add vercel.json

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,21 +1,24 @@
-from backend.extensions import db
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import select
-from werkzeug.security import generate_password_hash, check_password_hash
 from uuid import UUID, uuid4
 from datetime import datetime, timezone
 
+from sqlalchemy.orm import Mapped, mapped_column
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from backend.extensions import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    # SQLAlchemyの機能により、SQLiteの場合: CHAR(32) 型のカラムが作成され、
-    # UUIDはハイフンなしの32文字の文字列として保存されます。
+    # Pythonコード側では一貫して uuid.UUID オブジェクトとしてこのIDを扱うことができる。
+    # データベース側では、SQLAlchemyの機能により、SQLiteやmySQLの場合: CHAR(32) 型のカラムが作成され、
+    # UUIDはハイフンなしの32文字の文字列として保存される。。が、これを意識する必要はない
     id: Mapped[UUID] = mapped_column(db.Uuid(), primary_key=True, default=uuid4)
     username: Mapped[str] = mapped_column(db.String(30), unique=True)
     email: Mapped[str] = mapped_column(db.String(256), unique=True)
     password: Mapped[str] = mapped_column(db.String(256))
     is_admin: Mapped[bool] = mapped_column(db.Boolean(), default=False)
-    tokens_valid_from: Mapped[datetime] = mapped_column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    # パスワード変更時、「全デバイスからログアウト」機能、セキュリティインシデント対応などで役に立つ
+    tokens_valid_from: Mapped[datetime] = mapped_column(db.DateTime(timezone=True),
+                                                        default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return f'<User id:{self.id}, username:"{self.username}", email:"{self.email}">'
@@ -28,12 +31,13 @@ class User(db.Model):
         return check_password_hash(self.password, raw_password)
 
 
-
 class TokenBlocklist(db.Model):
     __tablename__ = 'blocked_tokens'
     id: Mapped[int] = mapped_column(db.Integer(), primary_key=True)
+    # UUIDを使うようなので、ハイフンも合わせて36文字というぴったりな範囲にはめている
     jti: Mapped[str] = mapped_column(db.String(36), index=True)
-    created_at: Mapped[datetime] = mapped_column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    created_at: Mapped[datetime] = mapped_column(db.DateTime(timezone=True),
+                                                default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return f"<Token jti:{self.jti}, created_at:{self.created_at}>"
